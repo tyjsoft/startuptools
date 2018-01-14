24e2243f60719b8c4bca6e3813464b1d9b4565f2
Ben Nowak <bennowak@Laurens-MacBook-Pro.local>
2018-01-13 23:44:47 -0500

    Added eslint to development environment and updated .eslintrc

--- es6/test.js
+++ es6/test.js
@@ -1,15 +1,15 @@
-'use string';
+"use string";
 // es6 feature: block-scoped "let" declaration
 const sentences = [
-    { subject: 'JavaScript', verb: 'is', object: 'great' },
-    { subject: 'Elephants', verb: 'are', object: 'large' },
+	{ subject: "JavaScript", verb: "is", object: "great" },
+	{ subject: "Elephants", verb: "are", object: "large" },
 ];
 // es6 feature: object destructuring
 function say({ subject, verb, object}) {
-    // es6 feature: template strings
-    console.log(`${subject} ${verb} ${object}`);
+	// es6 feature: template strings
+	console.log(`${subject} ${verb} ${object}`);
 }
 // es6 feature: for..of (or 'enhanced' for loop)
 for(let s of sentences) {
-    say(s);
+	say(s);
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-// Gulp depencdencies go here
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
+    // Run ESLint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+      .pipe(eslint())
+      .pipe(eslint.format());
     // Node source
     gulp.src("es6/**/*.js")
       .pipe(babel())
